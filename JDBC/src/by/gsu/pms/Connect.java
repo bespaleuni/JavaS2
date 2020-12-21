21968f1d1c4d0f31a5c00c7170da912a749290c6
bespaleuni <kuzminiwe@mail.ru>
2020-12-21 03:04:16 +0300

    first upload

---
+++ JDBC/src/by/gsu/pms/Connect.java
@@ -0,0 +1,31 @@
+package by.gsu.pms;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.SQLException;
+
+public class Connect {
+    private static Connect serviceDb = null;
+    private static MysqlDataSource dataSource;
+
+    private Connect(String dbName, String user, String password) {
+        final String connectionHeader = "jdbc:mysql://localhost:3306/";
+        final String connectionFooter = "?serverTimezone=Europe/Minsk";
+
+        dataSource = new MysqlDataSource();
+        dataSource.setUrl(connectionHeader + "dbName" + connectionFooter);
+        dataSource.setUser("root");
+        dataSource.setPassword("");
+    }
+
+    public static void init(String dbName, String user, String password) {
+        if (serviceDb != null) {
+            return;
+        }
+        serviceDb = new Connect(dbName, user, password);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
